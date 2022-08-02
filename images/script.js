7027c41c706685ee25cd0563f7b68f032b276548
AnnickNdri <108307395+AnnickNdri@users.noreply.github.com>
2022-08-02 07:38:11 +0000

    Add files via upload

---
+++ images/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 7,
+   
+    slidesPerGroup: 7,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
